13f2acc28bf0f87613b046ff12185e637e67a99d
Nate <nathaniel_ramirez@icloud.com>
2023-11-19 17:05:20 -0800

    better css

--- js/index.js
+++ js/index.js
@@ -11,20 +11,11 @@ function changeStatus() {
 document.addEventListener("DOMContentLoaded", function () {
   const profile = document.querySelector(".profile");
   const button = document.getElementById("copy");
-  const icon = document.createElement("i");
-  icon.setAttribute("class", "fa-regular fa-clone");
-  button.appendChild(icon);
   button.addEventListener("click", (event) => {
-    navigator.clipboard.writeText(
+    copyText(
       `<div class="profile" data-image="img/example.gif" data-status="${profile.getAttribute(
         "data-status"
       )}"></div>`
     );
-    icon.setAttribute("class", "fa-solid fa-check");
-    icon.style.color = "#4ce964";
-    setTimeout(() => {
-      icon.setAttribute("class", "fa-regular fa-clone");
-      icon.style.color = "#fff";
-    }, 500);
   });
 });
