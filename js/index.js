bdfa7a42e7513227735796dcefc719bbbd0c0983
Nate <nathaniel_ramirez@icloud.com>
2023-11-20 10:26:11 -0800

    update

--- js/index.js
+++ js/index.js
@@ -21,29 +21,60 @@ if (location.search === "?step=2") {
   const step_1 = document.getElementById("step_1");
   const step_2 = document.getElementById("step-2");
   const e_404 = document.getElementById("404");
-  const completeCard = document.getElementById("completeCard");
+  const steps = document.getElementById("steps");
   e_404.remove();
   step_1.remove();
+  steps.remove()
   step_2.style.display = "block";
   // start
   const completeButton = document.getElementById("completeButton");
-  completeButton.addEventListener("click", complete);
-  function complete() {
-    completeCard.style.display = "flex";
+  completeButton.addEventListener("click", function () {
+    const completeCard = document.getElementById("completeCard");
+    completeCard.style.display = "inline-block";
+    completeCard.style.transform = "scale(0)";
     setTimeout(() => {
-      completeCard.style.transform = "scale(1)"
-    }, 50);
-  }
+      completeCard.style.transform = "scale(1.2)";
+      completeCard.style.opacity = "1";
+    }, 200);
+    setTimeout(() => {
+      completeCard.style.transform = "scale(.9)";
+    }, 400);
+    setTimeout(() => {
+      completeCard.style.transform = "scale(1)";
+    }, 600);
+  });
 } else if (location.search === "") {
   const step_1 = document.getElementById("step_1");
   const step_2 = document.getElementById("step-2");
   const e_404 = document.getElementById("404");
+  const steps = document.getElementById("steps");
   step_1.style.display = "inline-block";
+  steps.remove()
   e_404.remove();
   step_2.remove();
+} else if (location.search === "?steps") {
+  const step_1 = document.getElementById("step_1");
+  const step_2 = document.getElementById("step-2");
+  const e_404 = document.getElementById("404");
+  const steps = document.getElementById("steps");
+  steps.style.display = "inline-block";
+  e_404.remove();
+  step_2.remove();
+  step_1.remove()
 } else {
   const step_1 = document.getElementById("step_1");
   const step_2 = document.getElementById("step-2");
+  const steps = document.getElementById("steps");
+  steps.remove()
   step_2.remove();
   step_1.remove();
+}
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
