9f45b3f7f4789044ba875aee8c2849fad487517e
Nate <nathaniel_ramirez@icloud.com>
2023-11-19 22:15:36 -0800

    update

--- js/index.js
+++ js/index.js
@@ -8,14 +8,42 @@ function changeStatus() {
   profile.setAttribute("data-status", nextStatus);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+function generate() {
   const profile = document.querySelector(".profile");
-  const button = document.getElementById("copy");
-  button.addEventListener("click", (event) => {
-    copyText(
-      `<div class="profile" data-image="img/example.gif" data-status="${profile.getAttribute(
-        "data-status"
-      )}"></div>`
-    );
-  });
-});
+  copyText(
+    `<div class="profile" data-image="img/example.gif" data-status="${profile.getAttribute(
+      "data-status"
+    )}"></div>`
+  );
+}
+
+if (location.search === "?step=2") {
+  const step_1 = document.getElementById("step_1");
+  const step_2 = document.getElementById("step-2");
+  const e_404 = document.getElementById("404");
+  const completeCard = document.getElementById("completeCard");
+  e_404.remove();
+  step_1.remove();
+  step_2.style.display = "block";
+  // start
+  const completeButton = document.getElementById("completeButton");
+  completeButton.addEventListener("click", complete);
+  function complete() {
+    completeCard.style.display = "flex";
+    setTimeout(() => {
+      completeCard.style.transform = "scale(1)"
+    }, 50);
+  }
+} else if (location.search === "") {
+  const step_1 = document.getElementById("step_1");
+  const step_2 = document.getElementById("step-2");
+  const e_404 = document.getElementById("404");
+  step_1.style.display = "inline-block";
+  e_404.remove();
+  step_2.remove();
+} else {
+  const step_1 = document.getElementById("step_1");
+  const step_2 = document.getElementById("step-2");
+  step_2.remove();
+  step_1.remove();
+}
